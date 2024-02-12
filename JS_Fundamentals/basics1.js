6468bd485992d1cd0460bca0027896077d23fa7c
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2024-02-12 11:22:20 -0800

    datatypes

--- JS_Fundamentals/basics1.js
+++ JS_Fundamentals/basics1.js
@@ -2,6 +2,16 @@ console.log("Hello World");
 
 let a = 4;
 console.log(a);
+console.log(typeof a);
+
+let b = 2.34;
+console.log(typeof b);
+
+let c = "Patrick Kelliher";
+console.log(typeof c);
+
+let required = true;
+console.log(typeof required);
 
 //there are comments
 /*

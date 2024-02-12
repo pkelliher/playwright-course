5447b4b34c9a24d9c75b4de654bb9ff0fff7e3a3
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2024-02-12 13:44:45 -0800

    for loop

--- JS_Fundamentals/basics2.js
+++ JS_Fundamentals/basics2.js
@@ -12,3 +12,13 @@ while (i < 10) {
   i++;
   console.log(i);
 }
+
+do {
+  i++;
+} while (i < 20);
+console.log(1);
+
+for (let k = 0; k <= 10; k++) {
+  console.log(k);
+}
+//Run this when you know how many times you will need to loop.

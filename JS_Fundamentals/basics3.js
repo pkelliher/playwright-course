93a68891239a833171dfc21b73d81c5ab81052bf
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2024-02-12 14:16:30 -0800

    arrays

---
+++ JS_Fundamentals/basics3.js
@@ -0,0 +1,14 @@
+var marks = Array(6);
+let grades = Array(20, 40, 35, 12, 37, 100);
+
+var marks = [20, 40, 35, 12, 37, 100];
+console.log(marks[2]);
+marks[3] = 14;
+console.log(marks);
+console.log(marks.length);
+marks.push(65);
+console.log(marks);
+marks.pop();
+console.log(marks);
+marks.unshift(12);
+console.log(marks);

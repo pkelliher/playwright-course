be254200e799f07f026f456d15de7128c00a59f1
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2024-02-12 13:55:30 -0800

    for loop with break after 30

--- JS_Fundamentals/basics2.js
+++ JS_Fundamentals/basics2.js
@@ -22,3 +22,19 @@ for (let k = 0; k <= 10; k++) {
   console.log(k);
 }
 //Run this when you know how many times you will need to loop.
+let required = true;
+while (required) {
+  console.log(required);
+  required = false;
+}
+//If true or false run while loop.
+let n = 0;
+for (k = 1; k <= 100; k++) {
+  if (k % 2 == 0 && k % 5 == 0) {
+    n++;
+    console.log(k);
+    if (n == 3) {
+      break;
+    }
+  }
+}

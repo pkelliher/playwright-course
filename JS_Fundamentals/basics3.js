be059af3d81d11197bd997300017046e8ad0a339
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2024-02-12 15:49:33 -0800

    filter, reduce, map

--- JS_Fundamentals/basics3.js
+++ JS_Fundamentals/basics3.js
@@ -23,3 +23,30 @@ for (let i = 0; i < marks.length; i++) {
   sum = sum + marks[i];
 }
 console.log(sum);
+
+//reduce filter map
+let total = marks.reduce((sum, mark) => sum + mark, 0);
+console.log(total);
+var scores = [12, 13, 14, 16];
+//create new array with only even numbers
+var evenScores = [];
+for (let i = 0; i < scores.length; i++) {
+  if (scores[i] % 2 == 0) {
+    evenScores.push(scores[i]);
+  }
+}
+console.log(evenScores);
+let newFilterEventScores = scores.filter((score) => score % 2 == 0);
+console.log(newFilterEventScores);
+//map
+let mappedArray = newFilterEventScores.map((score) => score * 3);
+console.log(mappedArray);
+
+let totalVal = mappedArray.reduce((sum, val) => sum + val, 0);
+console.log(totalVal);
+
+var scores1 = [12,13,14,16]
+
+let sumValues = scores.filter(score => score % 2 == 0).map(score => score * 3).reduce((sum, val) => sum + val, 0);
+console.log(sumValues);
+

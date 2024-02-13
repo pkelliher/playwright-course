cc3dd447161223a6f5cbe226d2321ada62802627
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2024-02-13 09:45:46 -0800

    UIBasicstest.spec.js

---
+++ PkPlayWright/playwright.config.js
@@ -0,0 +1,15 @@
+// @ts-check
+const { defineConfig, devices } = require("@playwright/test");
+
+module.exports = defineConfig({
+  testDir: "./tests",
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000,
+  },
+
+  reporter: "html",
+  use: {
+    browserName: "chromium",
+  },
+});

e1003669cfecdb91a58276cfe64820a7573ce4ea
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2024-02-14 17:52:04 -0800

    fill form with locators and click event

--- PkPlayWright/playwright.config.js
+++ PkPlayWright/playwright.config.js
@@ -10,7 +10,7 @@ module.exports = defineConfig({
 
   reporter: "html",
   use: {
-    browserName: "webkit",
+    browserName: "chromium",
     headless: false,
   },
 });
--- PkPlayWright/tests/UIBasicstest.spec.js
+++ PkPlayWright/tests/UIBasicstest.spec.js
@@ -5,10 +5,15 @@ test("Browser Context Playwright test", async ({ browser }) => {
   const page = await context.newPage();
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
   console.log(await page.title());
+  await page.locator("#username").fill("raulshetty");
+  await page.locator("[type='password]").fill("learning");
+  await page.locator("#signInBtn]").click();
 });
 
 test("Page Playwright test", async ({ page }) => {
   await page.goto("https://google.com");
   console.log(await page.title());
   await expect(page).toHaveTitle("Google");
+  //css
+  page.locator();
 });

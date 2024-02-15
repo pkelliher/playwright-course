756e95c85835427a9dde2b0a539863155152e4c7
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2024-02-14 18:08:34 -0800

    error textContent

--- PkPlayWright/tests/UIBasicstest.spec.js
+++ PkPlayWright/tests/UIBasicstest.spec.js
@@ -1,13 +1,14 @@
 const { test, expect } = require("@playwright/test");
 
-test("Browser Context Playwright test", async ({ browser }) => {
+test.only("Browser Context Playwright test", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
   console.log(await page.title());
   await page.locator("#username").fill("raulshetty");
-  await page.locator("[type='password]").fill("learning");
-  await page.locator("#signInBtn]").click();
+  await page.locator("[type='password']").fill("learning");
+  await page.locator("#signInBtn").click();
+  console.log(await page.locator("[style*='block']").textContent());
 });
 
 test("Page Playwright test", async ({ page }) => {

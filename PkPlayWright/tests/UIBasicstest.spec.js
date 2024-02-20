ddebe0ee736857effafddfd3096de9d00866fb04
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2024-02-19 18:06:23 -0800

    allTextContent does not wait for the DOM to load

--- PkPlayWright/tests/UIBasicstest.spec.js
+++ PkPlayWright/tests/UIBasicstest.spec.js
@@ -5,6 +5,7 @@ test.only("Browser Context Playwright test", async ({ browser }) => {
   const page = await context.newPage();
   const userName = page.locator("#username");
   const signIn = page.locator("#signInBtn");
+  const cardTitles = page.locator(".card-body a");
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
   console.log(await page.title());
   await userName.fill("rahulshetty");
@@ -15,10 +16,12 @@ test.only("Browser Context Playwright test", async ({ browser }) => {
   await userName.fill("");
   await userName.fill("rahulshettyacademy");
   await signIn.click();
-  console.log(await page.locator(".card-body a").first().textContent());
-  console.log(await page.locator(".card-body a").nth(1).textContent());
+  console.log(await cardTitles.first().textContent());
+  console.log(await cardTitles.nth(1).textContent());
+  const allTitles = await cardTitles.allTextContents();
+  console.log(allTitles);
 });
-
+ff
 test("Page Playwright test", async ({ page }) => {
   await page.goto("https://google.com");
   console.log(await page.title());

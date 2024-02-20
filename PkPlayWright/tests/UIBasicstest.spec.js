856609e816e874df66215e6e02b1925e1d475539
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2024-02-19 17:41:50 -0800

    using .nth and .first with textContent

--- PkPlayWright/tests/UIBasicstest.spec.js
+++ PkPlayWright/tests/UIBasicstest.spec.js
@@ -3,13 +3,20 @@ const { test, expect } = require("@playwright/test");
 test.only("Browser Context Playwright test", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
+  const userName = page.locator("#username");
+  const signIn = page.locator("#signInBtn");
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
   console.log(await page.title());
-  await page.locator("#username").fill("raulshetty");
+  await userName.fill("rahulshetty");
   await page.locator("[type='password']").fill("learning");
-  await page.locator("#signInBtn").click();
+  await signIn.click();
   console.log(await page.locator("[style*='block']").textContent());
   await expect(page.locator("[style*='block']")).toContainText("Incorrect");
+  await userName.fill("");
+  await userName.fill("rahulshettyacademy");
+  await signIn.click();
+  console.log(await page.locator(".card-body a").first().textContent());
+  console.log(await page.locator(".card-body a").nth(1).textContent());
 });
 
 test("Page Playwright test", async ({ page }) => {
